3a4eb6ade90fad177576cfed20ee5a7e7a734ede
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-05 14:51:03 -0400

    added comment header and began working on main.js
    added both constants and started the for loop

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -5,10 +5,13 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const alternativeImageText = ["Closeup of a blue human eye", "A rock formation", "Some purple and white flowers", "Egyptian hieroglyphics", "A butterfly on a leaf"];
 
 /* Looping through images */
+for (let i = 0; i < filenames.length; )
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
