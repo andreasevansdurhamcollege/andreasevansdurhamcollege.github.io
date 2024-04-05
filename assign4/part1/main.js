4eb71cdd68bfb07623d507b97973a58eacec4cf8
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-05 01:25:45 -0400

    added in result function and started editing it

--- assign4/part1/main.js
+++ assign4/part1/main.js
@@ -12,3 +12,26 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
