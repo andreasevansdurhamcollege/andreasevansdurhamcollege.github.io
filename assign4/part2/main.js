fb4500b626849f460db939e91e490454ea7615ab
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-08 18:23:50 -0400

    wrote the code for darken button but its not working and im not sure why
    moving on

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -24,3 +24,17 @@ for (const image of images) {
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+    const buttonClass = btn.getAttribute("class");
+    if (buttonClass === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent("Lighten");
+        overlay.style.backgroundColor = "rgba(0 0 0 / 50%)";
+
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent("Darken");
+        overlay.style.backgroundColor = "rgba(0 0 0 / 0%)";
+    }
+
+})
