b42a890afd482632e22812f5d7ebfed5133db7f3
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-08 17:55:51 -0400

    fixed the for loop finally

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -5,17 +5,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
-const alternativeImageText = ["Closeup of a blue human eye", "A rock formation", "Some purple and white flowers", "Egyptian hieroglyphics", "A butterfly on a leaf"];
+const alternativeImageText = {"pic1.jpg" : "Closeup of a blue human eye", "pic2.jpg" : "A rock formation", "pic3.jpg" : "Some purple and white flowers", "pic4.jpg" : "Egyptian hieroglyphics", "pic5.jpg" : "A butterfly on a leaf"};
 
 /* Looping through images */
-for (let i = 0; i < filenames.length; )
+for (const image of images) {
+    const newImage = document.createElement("img");
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute("alt", alternativeImageText[image]);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', event => {
+        displayedImage.src = event.target.src;
+        displayedImage.alt = event.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
