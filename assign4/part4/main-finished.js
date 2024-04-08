44da67552a2940c4d513b1da8c28976d808deff1
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-08 19:13:00 -0400

    Update main-finished.js
    adding functions to EvilCircle

--- assign4/part4/main-finished.js
+++ assign4/part4/main-finished.js
@@ -85,7 +85,50 @@ class EvilCircle extends Shape {
         super(x, y, 20, 20);
         this.color = "white";
         this.size = 10;
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+              case "a":
+                this.x -= this.velX;
+                break;
+              case "d":
+                this.x += this.velX;
+                break;
+              case "w":
+                this.y -= this.velY;
+                break;
+              case "s":
+                this.y += this.velY;
+                break;
+            }
+          });    
     }
+
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+      }
+
+    checkBounds() {
+        if (this.x + this.size >= width) {
+          this.velX = -Math.abs(this.velX);
+        }
+    
+        if (this.x - this.size <= 0) {
+          this.velX = Math.abs(this.velX);
+        }
+    
+        if (this.y + this.size >= height) {
+          this.velY = -Math.abs(this.velY);
+        }
+    
+        if (this.y - this.size <= 0) {
+          this.velY = Math.abs(this.velY);
+        }
+      }
 }
 
 const balls = [];
