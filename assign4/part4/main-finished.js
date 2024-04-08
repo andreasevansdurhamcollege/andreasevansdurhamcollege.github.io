76b5447667966333f2a0fa6196ddd82f914c7943
andreasevansdurhamcollege <andreas.evansadamecz@dcmail.ca>
2024-04-08 19:05:51 -0400

    Update main-finished.js
    finished Shape and Ball class now working on EvilCircle

--- assign4/part4/main-finished.js
+++ assign4/part4/main-finished.js
@@ -18,18 +18,23 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Shape extends Ball {
+class Shape {
+    constructor(x, y, velX, velY) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
 
 }
 
-class Ball {
+class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
+
   }
 
   draw() {
@@ -62,7 +67,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -73,6 +78,14 @@ class Ball {
       }
     }
   }
+}
+
+class EvilCircle extends Shape {
+    constructor(x, y) {
+        super(x, y, 20, 20);
+        this.color = "white";
+        this.size = 10;
+    }
 }
 
 const balls = [];
